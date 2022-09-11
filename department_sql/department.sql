5690c45c9925149735ec4a507afc553edfe6ab48
daliakhalifa02 <daliakhalifa02@gmail.com>
2022-09-11 23:24:21 +0300

    query 4

--- department_sql/department.sql
+++ department_sql/department.sql
@@ -12,3 +12,7 @@ WHERE start_time=(SELECT MIN(start_time) FROM courses);
 SELECT DISTINCT name
 FROM   students
 WHERE id NOT IN (SELECT DISTINCT student_id FROM enrolled);
+
+/* counting the number of CS students enrolled in any course */
+SELECT count(*) FROM enrolled WHERE student_id in 
+(SELECT student_id FROM majorsln WHERE dept_id in (SELECT id FROM departments WHERE name = 'CS'))
